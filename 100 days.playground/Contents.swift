fd4aab9ce362f3bb75a7fc472b0984fac8696d17
ericuxnyc <86964628+ericuxnyc@users.noreply.github.com>
2024-08-09 09:38:00 -0400

    Initial Commit

---
+++ 100 days.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+import SwiftUI
+
+let celsius: Double = 0
+var fahrenheit: Double {
+   (celsius * 9/5) + 32
+}
+
+print("\(fahrenheit)° F, \(celsius)° C")
+
+func ready(){
+    print("Testing")
+    print("Ready to")
+    print("Code for Money")
+}
+ready()
+
+var person = (name: "Eric", age: 40, isMarried: true)
+
+print("My Name is \(person.name)")
+
+func WhoAmI(name: String, age: Int, isMarried: Bool){
+    print("My Name is \(name)")
+    print("I am \(age) years old")
+    print("I am married: \(isMarried)")
+}
+
+WhoAmI(name: "Carmen", age: 40, isMarried: true)
+
+
+
